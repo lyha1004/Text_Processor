68da957617f46ed3d6ed026817ddd6181107288e
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-10-02 13:40:31 -0500

    adding test case inocrrect spelling

--- src/tests.py
+++ src/tests.py
@@ -6,29 +6,21 @@ class Tests(unittest.TestCase):
         self.assertTrue(True)
 
     def test_empty_string_returns_empty_string(self):
-<<<<<<< Updated upstream
-        text_processor = TextProcessor()
-        text = ""
-        actual_ouput = text_processor.process_file(text)
-
-        self.assertEqual(text, actual_ouput)
-
-    def test_hello_returns_hello(self):
-        text_processor = TextProcessor()
-        text = "hello"
-        actual_ouput = text_processor.process_file(text)
-
-        self.assertEqual(text, actual_ouput)
-        
-=======
        actual_output = process_file("")
+       
        self.assertEqual("", actual_output)
 
     def test_hello_returns_hello(self):
         actual_output = process_file("hello")
+        
         self.assertEqual("hello", actual_output)
        
->>>>>>> Stashed changes
+    def test_incorrect_spelling_returns_brackets(self):
+        actual_output = process_file("blah")
+
+        self.assertEqual("[blah]", actual_output)
+
 
 if __name__ == '__main__': 
   unittest.main()
+       
--- src/text_processor.py
+++ src/text_processor.py
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-class TextProcessor:
-    
-    def process_file(self, text):
-        return text
-=======
-
 def process_file(text):
     return text
->>>>>>> Stashed changes
+
