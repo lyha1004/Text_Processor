fb918dfa630b311deb7305ef5d93a9dc49f0f2c8
Ly Ha <lythituyetha@gmail.com>
2023-10-01 20:04:59 -0500

    updating indents

--- src/tests.py
+++ src/tests.py
@@ -5,20 +5,20 @@ class Tests(unittest.TestCase):
     def test_canary(self):
         self.assertTrue(True)
 
-def test_empty_string_returns_empty_string(self):
-    text_processor = TextProcessor()
-    text = ""
-    actual_ouput = text_processor.process_file(text)
+    def test_empty_string_returns_empty_string(self):
+        text_processor = TextProcessor()
+        text = ""
+        actual_ouput = text_processor.process_file(text)
 
-    self.assertEqual(text, actual_ouput)
+        self.assertEqual(text, actual_ouput)
 
-def test_hello_returns_hello(self):
-    text_processor = TextProcessor()
-    text = "hello"
-    actual_ouput = text_processor.process_file(text)
+    def test_hello_returns_hello(self):
+        text_processor = TextProcessor()
+        text = "hello"
+        actual_ouput = text_processor.process_file(text)
 
-    self.assertEqual(text, actual_ouput)
-       
+        self.assertEqual(text, actual_ouput)
+        
 
 if __name__ == '__main__': 
   unittest.main()
