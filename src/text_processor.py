c8ebb41488fc5d2e65127dc454356febf6ddcc79
Ly Ha <lythituyetha@gmail.com>
2023-10-01 20:02:14 -0500

    updates

--- src/text_processor.py
+++ src/text_processor.py
@@ -1,12 +1,4 @@
-import os
-
 class TextProcessor:
     
-    def process_file(self, file_path):
-        if not os.path.isfile(file_path):
-            raise FileNotFoundError("File not found")
-        
-        with open(file_path, 'r') as file:
-            file_contents = file.read()
-
-        return file_contents
+    def process_file(self, text):
+        return text
