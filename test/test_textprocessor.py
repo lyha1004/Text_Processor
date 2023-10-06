7af3d6141ba11f107779435a43b34886a8398a25
Ly Ha <lythituyetha@gmail.com>
2023-10-05 22:56:31 -0500

    fixing compilation error

--- test/test_textprocessor.py
+++ test/test_textprocessor.py
@@ -15,7 +15,7 @@ class Tests(unittest.TestCase):
         actual_output = process_file("hello", is_valid_word)
         self.assertEqual("hello", actual_output)
   
-    @patch('text_processor.is_valid_word')
+    @patch('src.text_processor.is_valid_word')
     def test_incorrect_spelling_returns_brackets(self, mock_is_valid_word):
         mock_is_valid_word.side_effect = lambda text: False if text == "blah" else True
         actual_output = process_file("blah", mock_is_valid_word)
