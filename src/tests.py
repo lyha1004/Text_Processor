04c3f328a33e493f2f80d948bd5e238f6025d2c0
Ly Ha <lythituyetha@gmail.com>
2023-10-05 21:51:08 -0500

    fixing things

--- src/tests.py
+++ src/tests.py
@@ -15,11 +15,11 @@ class Tests(unittest.TestCase):
         actual_output = process_file("hello", is_valid_word)
         self.assertEqual("hello", actual_output)
   
-    def test_incorrect_spelling_returns_brackets(self):
-        with patch('text_processor.is_valid_word') as mock_is_valid_word:
-            mock_is_valid_word.side_effect = lambda text: False if text == "blah" else True
-            actual_output = process_file("blah", mock_is_valid_word)
-            self.assertEqual("[blah]", actual_output)
+    @patch('text_processor.is_valid_word')
+    def test_incorrect_spelling_returns_brackets(self, mock_is_valid_word):
+        mock_is_valid_word.side_effect = lambda text: False if text == "blah" else True
+        actual_output = process_file("blah", mock_is_valid_word)
+        self.assertEqual("[blah]", actual_output)
 
 
 if __name__ == '__main__': 
--- test/test_textprocessor.py
+++ test/test_textprocessor.py
@@ -18,7 +18,7 @@ class Tests(unittest.TestCase):
     def test_incorrect_spelling_returns_brackets(self):
         with patch('text_processor.is_valid_word') as mock_is_valid_word:
             mock_is_valid_word.side_effect = lambda text: False if text == "blah" else True
-            actual_output = process_file("blah", mock_is_valid_word)
+            actual_output = process_file("blah", is_valid_word)
             self.assertEqual("[blah]", actual_output)
 
 
