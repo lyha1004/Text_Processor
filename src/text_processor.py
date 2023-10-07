a72d5d0cba13d5bf09fa5a7405a5ae74ef49e4d9
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-10-07 15:30:19 -0500

    adding test cases

--- src/text_processor.py
+++ src/text_processor.py
@@ -1,5 +1,11 @@
 
-def process_text(text, check_spelling):
-    return text if check_spelling(text) else f"[{text}]"
-
+def process_text(text, *check_spelling):
+    result = []
+    words = text.split()
+    for word, check_spelling in zip(words, check_spelling):
+        if check_spelling(word):
+            result.append(word)
+        else:
+            result.append(f"[{word}]")
+    return " ".join(result)
 
--- test/test_textprocessor.py
+++ test/test_textprocessor.py
@@ -2,24 +2,20 @@ import unittest
 from unittest.mock import Mock
 from src.text_processor import process_text
 
-
 class Tests(unittest.TestCase):
     def test_canary(self):
         self.assertTrue(True)
 
     def test_empty_string_returns_empty_string(self):
        check_spelling = Mock(return_value=True)
-
        self.assertEqual('', process_text('', check_spelling))
 
     def test_hello_returns_hello(self):
         check_spelling = Mock(return_value=True)
-
         self.assertEqual('hello', process_text('hello', check_spelling))
   
     def test_incorrect_spelling_returns_brackets(self):
         check_spelling = Mock(return_value=False)
-        
         self.assertEqual("[blah]", process_text('blah', check_spelling))
 
 
