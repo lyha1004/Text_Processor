c541da5c499a15999e8d4af2f3d5fac4e118a7b9
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-10-06 20:19:55 -0500

    implementing feedback

--- src/tests.py
+++ src/tests.py
@@ -1,25 +1,23 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock
 from text_processor import process_text
-from spell_checker import check_spelling
+
 
 class Tests(unittest.TestCase):
     def test_canary(self):
         self.assertTrue(True)
 
     def test_empty_string_returns_empty_string(self):
-       actual_output = process_text("", check_spelling)
-       self.assertEqual("", actual_output)
+       check_spelling = Mock(return_value=True)
+       self.assertEqual('', process_text('', check_spelling))
 
     def test_hello_returns_hello(self):
-        actual_output = process_text("hello", check_spelling)
-        self.assertEqual("hello", actual_output)
+        check_spelling = Mock(return_value=True)
+        self.assertEqual('hello', process_text('hello', check_spelling))
   
-    @patch('spell_checker.check_spelling')
-    def test_incorrect_spelling_returns_brackets(self, mock_check_spelling):
-        mock_check_spelling.return_value = False
-        actual_output = process_text("blah", mock_check_spelling)
-        self.assertEqual("[blah]", actual_output)
+    def test_incorrect_spelling_returns_brackets(self):
+        check_spelling = Mock(return_value=False)
+        self.assertEqual("[blah]", process_text('blah', check_spelling))
 
 
 if __name__ == '__main__': 
