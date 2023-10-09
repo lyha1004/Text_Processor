479f80fc9ed5c4169988d8223725ca3039cf752b
Ly Ha <lythituyetha@gmail.com>
2023-10-09 17:02:48 -0500

    updating for paver

--- test/test_textprocessor.py
+++ test/test_textprocessor.py
@@ -12,27 +12,72 @@ class Tests(unittest.TestCase):
 
     def test_hello_returns_hello(self):
         check_spelling = Mock(return_value=True)
+        
         self.assertEqual('hello', process_text('hello', check_spelling))
   
     def test_incorrect_spelling_returns_brackets(self):
         check_spelling = Mock(return_value=False)
+        
         self.assertEqual("[blah]", process_text('blah', check_spelling))
 
     def test_hello_there_returns_hello_there(self):
-        check_spelling = Mock()
-        check_spelling.side_effect = [True, True]
+        check_spelling = Mock(side_effect = [True, True])
+        
         self.assertEqual("hello there", process_text("hello there", check_spelling))
 
     def test_hello_tyop_returns_hello_with_bracket_tyop(self):
-        check_spelling = Mock()
-        check_spelling.side_effect = [True, False]
+        check_spelling = Mock(side_effect = [True, False])
+        
         self.assertEqual("hello [tyop]", process_text("hello tyop", check_spelling))
 
     
     def test_misp_hello_returns_bracket_misp_hello(self):
-        check_spelling = Mock()
-        check_spelling.side_effect = [False, True]
+        check_spelling = Mock(side_effect = [False, True])
+
         self.assertEqual("[misp] hello", process_text("misp hello", check_spelling))
+
+    def test_hello_tyop_there_misp_returns_two_brackets(self):
+
+        check_spelling =  Mock(side_effect = [word not in ['tyop', 'misp'] for word in "hello tyop there misp".split()])
+        
+        self.assertEqual("hello [tyop] there [misp]", process_text("hello tyop there misp", check_spelling))
+
+    def test_two_lines_returns_two_lines(self):
+
+        text = """line one
+line two"""
+        check_spelling = Mock(return_value = True)
+
+        self.assertEqual(text, process_text(text, check_spelling))
+
+    def test_two_lines_with_incorrect_spelling_returns_two_lines_with_brackets(self):
+        text = """cool ctas
+ctue dogs"""
+
+        check_spelling = Mock(side_effect = [word not in ['ctas', 'ctue'] for word in """cool ctas
+ctue dogs""".split()])
+
+        self.assertEqual("cool [ctas]\n[ctue] dogs", process_text(text, check_spelling))
+
+    def test_three_lines_returns_three_lines(self):
+        text = """line one
+line two
+line three"""
+
+        check_spelling = Mock(return_value = True)
+
+        self.assertEqual(text, process_text(text, check_spelling))
+
+    def test_three_lines_with_incorrect_spelling_returns_correct_results(self):
+        text = """cool ctas
+ctue dogs
+crazy horzes"""
+
+        check_spelling = Mock(side_effect = [word not in ['ctas', 'ctue', 'horzes'] for word in """cool ctas
+ctue dogs
+crazy horzes""".split()])
+
+        self.assertEqual("cool [ctas]\n[ctue] dogs\ncrazy [horzes]", process_text(text, check_spelling))
 
 if __name__ == '__main__': 
   unittest.main()
