9f62aabe852b736a61f233f740e6dcbe657570b0
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-10-12 18:46:47 -0500

    console

--- src/console_UI.py
+++ src/console_UI.py
@@ -3,3 +3,14 @@
 #read text from file
 #print spellchecked text to console
 #provide task to run from build
+
+#file_name = input("Enter file name:\n")
+
+
+if __name__ == "__main__":
+
+    with open("sample.txt") as file: 
+        text=file.read()
+    print(text)
+
+    
