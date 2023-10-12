8b04022895ccaf929fc78c3b3534be4fa2459dd8
Ly Ha <lythituyetha@gmail.com>
2023-10-12 16:46:12 -0500

    fixing spaces

--- src/spell_checker.py
+++ src/spell_checker.py
@@ -1,17 +1,17 @@
 import requests
 
 def get_Response(word):
-   url = f'http://agilec.cs.uh.edu/spell?check={word}'
-   response = requests.get(url)
-   return response
+  url = f'http://agilec.cs.uh.edu/spell?check={word}'
+  response = requests.get(url)
+  return response
 
 def parse_Text(response):
-   if response == 'true':
-      return True
-   elif response == 'false':
-      return False
+  if response == 'true':
+    return True
+  elif response == 'false':
+    return False
 
 def check_spelling(word):
-   response = get_Response(word)
-   parsed_response = parse_Text(response)
-   return parsed_response
+  response = get_Response(word)
+  parsed_response = parse_Text(response)
+  return parsed_response
