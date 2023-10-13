4056e4b9e071180dcd7c070317169f66297bb9eb
Ly Ha <lythituyetha@gmail.com>
2023-10-12 20:59:08 -0500

    getting rid of lines

--- src/console_UI.py
+++ src/console_UI.py
@@ -1,11 +1,5 @@
-#prompt user for file name +
-#take name of file as input +
-#read text from file +
-#print spellchecked text to console +
-#provide task to run from build
 from text_processor import process_text
 from spell_checker import check_spelling
-import sys
 
 def read_file(file_name):
     with open(f"src/input_files/{file_name}", "r") as file: 
@@ -14,12 +8,7 @@ def read_file(file_name):
 
 if __name__ == "__main__":
     file_name = input("Enter file name:\n")
-    
-    # if len(sys.argv) != 2:
-    #     print("Usage: python your_program.py <file_name>")
-    #     sys.exit(1)
 
-    # file_name = sys.argv[1]
     text = read_file(file_name)
     print(process_text(text, check_spelling))
 
