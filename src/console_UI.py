f4e9f370f69c6e8543f13cd694d1fd4ddcd938fa
Ly Ha <lythituyetha@gmail.com>
2023-10-12 19:03:11 -0500

    working on console UI

--- src/console_UI.py
+++ src/console_UI.py
@@ -1,16 +1,20 @@
-#prompt user for file name
-#take name of file as input
-#read text from file
-#print spellchecked text to console
+#prompt user for file name +
+#take name of file as input +
+#read text from file +
+#print spellchecked text to console +
 #provide task to run from build
+from text_processor import process_text
+from spell_checker import check_spelling
 
-#file_name = input("Enter file name:\n")
-
+def read_file(file_name):
+    with open(file_name, "r") as file: 
+        text = file.read()
+        return text
 
 if __name__ == "__main__":
+    file_name = input("Enter file name:\n")
+    text = read_file(file_name)
+    print(process_text(text, check_spelling))
 
-    with open("sample.txt") as file: 
-        text=file.read()
-    print(text)
 
     
